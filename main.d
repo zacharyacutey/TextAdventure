1266482857d1f64a0ea57ca2042195f5afa6ef82
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-27 18:18:18 -0400

    Update main.d

--- main.d
+++ main.d
@@ -1,2 +1,19 @@
 import std.stdio;
-int[int[4]][] map;
+import std.random;
+int[int[3]][] map; //Game Map
+int[3] pos; //Player position
+
+enum {
+  GRASS, //Ground level nothing
+  ENEMY_GRASS, //Ground level enemy
+  CAVE, //Underground level nothing
+  ENEMY_CAVE, //Underground level enemy
+  ENTRANCE, //Ground level Descend
+  LADDER, //Underground level Ascend/Descend
+  BOSS //Boss Battle
+} //The enumeration of the possible values for the tile values
+
+void main()
+{
+  
+}
