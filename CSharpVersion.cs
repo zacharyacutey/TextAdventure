9ec0dd4a6c3f839eaf8c1ab32c0f2bc82253c302
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-14 17:44:26 -0400

    Update CSharpVersion.cs

--- CSharpVersion.cs
+++ CSharpVersion.cs
@@ -1,8 +1,9 @@
-import Random;
+using System;
 class TextAdventure {
+  //GENERAL UTILITIES
   public static Random RGI = new Random();
   public static int gen(int x,int y)
   {
-    return RGI.next(x,y+1);
+    return RGI.Next(x,y+1);
   }
 }
