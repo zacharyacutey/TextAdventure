62a3adcbbf34893f9cc1ad19f7bb59163c3b5e6e
Zacharý <zacharyacutey@users.noreply.github.com>
2016-09-19 16:43:22 -0400

    Update combat.d

--- combat.d
+++ combat.d
@@ -33,5 +33,94 @@ struct Entity
   string name;
   int health;
   int max_health;
-  int[2] dodge_fail;
+  int armor;
+  int[2] fail;
+  Weapon[] attacks;
+  int medkits;
+}
+void estats(Entity e)
+{
+  import std.stdio;
+  writeln(e.name ~ " health:\t",e.health);
+}
+void pstats(Entity e)
+{
+  import std.stdio;
+  writeln(e.name ~ " health:\t",e.health);
+  writeln("Max health:\t",e.max_health);
+  writeln("Medkits:\t",e.medkits);
+}
+void stats(Entity e,Entity p)
+{
+  estats(e);
+  pstats(p);
+}
+bool is_dead(Entity e)
+{
+  return e.health <= 0;
+}
+void do_damage(Entity harmer,Entity harmed,int weapon_index = 0)
+{
+  import std.stdio;
+  Weapon weapon = harmer.weapons[weapon_index];
+  bool fail_var = is_fail(weapon.fail);
+  if(fail_var)
+  {
+    writeln(harmer.name," missed!");
+  }
+  else
+  {
+    int damage = gen(harmer.min_damage,harmer.max_damage);
+    damage -= harmed.armor;
+    if(damage <= 0)
+    {
+      writeln("The armor of ",harmed.name," blocked the damage!");
+      damage = 0;
+    }
+    harmed.health -= damage;
+    writeln(harmer.name," attacked ",harmed.name," and gave ",damage," damage!");
+  }
+}
+void action_command(char command,Entity player,Entity enemy)
+{
+  switch(command)
+  {
+    case 'a':
+    case 'A':
+      do_damage(player,enemy);
+      if(is_dead(enemy)) return;
+      do_damage(enemy,player);
+      break;
+    case 'd':
+    case 'D':
+      if(is_fail(player.fail))
+      {
+        writeln("You try to dodge, but failed!");
+        do_damage(enemy,player);
+      }
+      else
+      {
+        writeln("You succesfully dodged!");
+      }
+      break;
+    case 'm':
+    case 'M':
+      if(player.medkits > 0)
+      {
+        writeln("You used a medkit! Health fully restored!");
+      }
+      else
+      {
+        writeln("You're out of medkits!");
+      }
+      do_damage(enemy,player);
+      break;
+    case '\n':
+    case '\r':
+      break;
+    default:
+      writeln("You sit there doing nothing! Giving your enemy a chance to attack!");
+      do_damage(enemy,player);
+      break;
+  }
 }
